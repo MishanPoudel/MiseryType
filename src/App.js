83ef0f78a82a255d98949d5ea2b9eaba33f1abab
Mishan Poudel <poudelmishan2@gmail.com>
2024-01-12 11:49:39 +0000

    feat: add WordContainer component with input logic

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import TextConfig from "./components/TextConfig";
+import WordContainer from "./components/WordContainer";
 
 function App() {
   return (
     <>
       <Navbar />
       <TextConfig />
+      <WordContainer/>
       {/* <About/> */}
       <Footer />
     </>
